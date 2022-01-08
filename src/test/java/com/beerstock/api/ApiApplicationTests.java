d906ae2022b00ce04afa16abf03a3719a7884be5
Luis Carlos <crluis521@gmail.com>
2022-01-08 16:53:34 -0300

    Commit com a utilização de testes

---
+++ src/test/java/com/beerstock/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerstock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
